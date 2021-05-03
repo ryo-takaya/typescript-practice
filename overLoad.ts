ef24819dfc36583d874e125da961db87f7e279a5
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-05-03 14:30:53 +0900

    create oberLoad.ts

---
+++ overLoad.ts
@@ -0,0 +1,12 @@
+export {}
+
+function double(value:number):number;
+function double(value:string):string;
+
+function double  (value:any):any{
+  return value * 2
+}
+
+// const double = (value:string):string => {
+//   return value + value
+// }

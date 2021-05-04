d08ac6499aa5e726d079edf87ed5c1c5069c5981
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-05-04 17:43:14 +0900

    create mappedTypes

---
+++ mappedTypes.ts
@@ -0,0 +1,30 @@
+export {}
+
+type Profile = {
+    name:string
+    age:number
+}
+
+type PartialProfile = Partial<Profile>
+type RequireProfile = Required<PartialProfile>
+
+const obj:PartialProfile = {
+    name:'kjkj'
+}
+
+type map = {
+    [P in keyof Profile]:Profile[P]
+}
+
+type ind = {
+    [index:string]:number
+}
+
+const k:ind = {}
+k[0] = 3
+
+type Optional<T> = {
+    [P in keyof T]?: T[P] | null
+}
+
+type Test = name in Profile

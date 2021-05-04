710fa83175886bd6bd0731391ac9556090a39448
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-05-04 09:47:49 +0900

    create abstract.ts

---
+++ abstract.ts
@@ -0,0 +1,23 @@
+export {}
+
+abstract class Animal{
+    abstract cry():string;
+}
+
+class Lion extends Animal{
+    cry(){
+        return 'gaon'
+    }
+
+    get unko(){
+        return 'unko'
+    }
+}
+
+class Tiger extends Animal{
+    cry(){
+        return 'gao'
+    }
+
+    ge
+}
